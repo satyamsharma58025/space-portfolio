ad076943dea94eb49314e0e2de79620b9dbb72a9
Satyam Sharma <161165084+satyamsharma58025@users.noreply.github.com>
2025-08-23 08:43:01 +0000

    edit

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -17,14 +17,7 @@ export const Navbar = () => {
           href="#about-me"
           className="flex items-center"
         >
-          <Image
-            src="/logo.png"
-            alt="Logo"
-            width={70}
-            height={70}
-            draggable={false}
-            className="cursor-pointer"
-          />
+          
           <div className="hidden md:flex md:selffont-bold ml-[10px] text-gray-300">Satyam Sharma </div>
         </Link>
 
