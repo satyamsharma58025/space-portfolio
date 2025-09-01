821c9b034216a1a965fe81e342038a34d7ac1b8e
Satyam Sharma <161165084+satyamsharma58025@users.noreply.github.com>
2025-09-01 13:22:15 +0000

    ok fine

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const siteConfig: Metadata = {
-  title: "Satyam Space Portfolio",
+  title: "Satyam Sharma Portfolio",
   description: "Welcome to my full stack Next.js 14 space portfolio.",
   keywords: [
     "reactjs",
